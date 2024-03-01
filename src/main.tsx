9f5938f3dd8fba18915caa3c11780da8867f4dc1
Joao Alegria <joao.alegria@openbrix.co.uk>
2024-03-01 00:53:02 +0000

    1st commit

---
+++ src/main.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      // -tells it to refetch the data after x time, this is global but it can be set on individual useQuery
+      staleTime: 1000 * 10 
+      // -amount of time between refetches
+      // refetchInterval: 5000
+    }
+  }
+})
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+        {/* <ReactQueryDevtools /> */}
+      </BrowserRouter>
+    </QueryClientProvider>
+  </React.StrictMode>,
+)
